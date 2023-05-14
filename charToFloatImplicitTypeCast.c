0fe60615815da3ecba9706b41c4ab4a608454d78
ifthekar <sajalcse2018@gmail.com>
2023-05-14 12:26:04 +0600

    char to float implicit type casting

---
+++ charToFloatImplicitTypeCast.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+     int a=21;
+     float result;
+
+     char ch='T';
+
+     result=ch+a;  //Implicit Type casting
+
+     printf("Result is::%f",result);
+
+
+return 0;
+}
