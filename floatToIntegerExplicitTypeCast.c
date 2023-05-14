bed3cce8e2462318c55c50beaf990624a673b177
ifthekar <sajalcse2018@gmail.com>
2023-05-14 12:48:04 +0600

    float ot integer cast

---
+++ floatToIntegerExplicitTypeCast.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+float floatValue=56.3;
+
+int value=(int)floatValue+50;
+
+printf("Before Casting::%f\n",floatValue);
+
+printf("After Casting::%d\n",value);
+
+
+return 0;
+
+
+}
